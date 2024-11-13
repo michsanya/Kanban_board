52480e0dec61bc0679671c956a90bc443576cd2f
Aleksandr Vinogradov <34797717+michsanya@users.noreply.github.com>
2024-11-13 17:43:38 +0300

    Some logic move to kanban.py

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import datetime
 import time
+from kanban import Board
 from collections import namedtuple
-
-shift = namedtuple("shift", "name, date, kanbans")
-shifts = {num: shift for num in range(14)}
 
 current_week_num = time.strftime("%U", time.gmtime())
 next_week_num = time.strftime("%U", time.gmtime(time.time() + 7 * 24 * 60 * 60))
 
 
-def get_dates_list(date: datetime.datetime | None = None) -> list[datetime.datetime]:
-    date_now = date if date else datetime.datetime.now()
-    first_date = date_now - datetime.timedelta(days=date_now.weekday())
-    dates = [first_date + datetime.timedelta(days=x) for x in range(14)]
-    return dates
+b = Board("Test_board")
+
+# print(b)
+# print(*b.date_field, sep="\n")
+
 
 
 #print(get_dates_list())
