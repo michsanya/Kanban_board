01b5516adbf2863ce4d309199be9d8fb0c27675d
Aleksandr Vinogradov <34797717+michsanya@users.noreply.github.com>
2024-11-13 17:42:25 +0300

    Create initialization for empty board with dates and without shift name

--- kanban.py
+++ kanban.py
@@ -1,7 +1,12 @@
 import datetime
-from main import get_dates_list
 from enum import Enum
 
+
+def get_dates_list(date: datetime.datetime | None = None) -> list[datetime.datetime]:
+    date_now = date if date else datetime.datetime.now()
+    first_date = date_now - datetime.timedelta(days=date_now.weekday())
+    dates = [first_date + datetime.timedelta(days=x) for x in range(14)]
+    return dates
 
 
 class Kanban:
@@ -22,18 +27,24 @@ class Board_Field:
         self.is_morning = is_morning
         # A,B,C or D
         self.shift_name = shift_name
-        self.kanbans = []
+    def __str__(self):
+        return f"{self.date.strftime('%d.%m')},{self.shift_name},{'D' if self.is_morning else 'N'}"
+    __repr__=__str__
 
 
 class Board:
     def __init__(self, name):
         self.name = name
-        self.date_field = main.get_dates_list
+        # self.date_field = [Board_Field(x, True, "Z") for x in get_dates_list()]
+        self.date_field = []
+        for x in get_dates_list(): 
+            self.date_field.append(Board_Field(x, True, "Z"))
+            self.date_field.append(Board_Field(x, False, "Z"))
+        
+        self.in_work = []
         self.tasks_list = []
         self.completed = []
 
     def create_kanban(self, text):
         self.tasks_list.append(Kanban("WO", text))
-
-bf = Board_Field(datetime.datetime.now(), True, "A")
 
