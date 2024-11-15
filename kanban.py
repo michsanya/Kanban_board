b8e61f93da82a08e72cab3258d8c514009b1aff7
Александр <michsanya1@gmail.com>
2024-11-16 00:46:02 +0300

    added get shift name

--- kanban.py
+++ kanban.py
@@ -9,6 +9,26 @@ def get_dates_list(date: datetime.datetime | None = None) -> list[datetime.datet
     return dates
 
 
+def get_day_shift_name(date: datetime.datetime,
+                       first_shift_of_first_shift: datetime.datetime,
+                       names: list[str] | None = None
+                       ) -> str:
+    names = names if names else ["A", "A", "B", "B", "C", "C", "D", "D"]
+    num_of_day = date.timetuple().tm_yday
+    delta = first_shift_of_first_shift.timetuple().tm_yday
+    return names[(num_of_day - delta) % len(names)]
+
+
+def get_night_shift_name(day_shift: str,
+                         names: list[str] | None = None
+                         ) -> str:
+    names_dict = {"A": "C",
+                  "B": "D",
+                  "C": "A",
+                  "D": "B"}
+    return names_dict[day_shift]
+
+
 class Kanban:
     def __init__(self, kanban_type, text):
         self.kanban_type = kanban_type
@@ -21,30 +41,33 @@ class Kanban:
 
 
 class Board_Field:
-    def __init__(self, date:datetime.datetime, is_morning:bool, shift_name:str):
+    def __init__(self, date: datetime.datetime, is_morning: bool, shift_name: str):
         self.date = date
         # day shift (7:30-19:30) or night shift (19:30-7:30)
         self.is_morning = is_morning
         # A,B,C or D
         self.shift_name = shift_name
+
     def __str__(self):
         return f"{self.date.strftime('%d.%m')},{self.shift_name},{'D' if self.is_morning else 'N'}"
-    __repr__=__str__
+
+    __repr__ = __str__
 
 
 class Board:
+    first_shift_of_first_shift = datetime.datetime(2024, 1, 2)
+
     def __init__(self, name):
         self.name = name
         # self.date_field = [Board_Field(x, True, "Z") for x in get_dates_list()]
         self.date_field = []
-        for x in get_dates_list(): 
-            self.date_field.append(Board_Field(x, True, "Z"))
-            self.date_field.append(Board_Field(x, False, "Z"))
-        
+        for x in get_dates_list():
+            self.date_field.append(Board_Field(x, True, n := get_day_shift_name(x, Board.first_shift_of_first_shift)))
+            self.date_field.append(Board_Field(x, False, get_night_shift_name(n)))
+
         self.in_work = []
         self.tasks_list = []
         self.completed = []
 
     def create_kanban(self, text):
         self.tasks_list.append(Kanban("WO", text))
-
--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import datetime
 import time
-from kanban import Board
+from kanban import Board, get_day_shift_name
 from collections import namedtuple
 
 current_week_num = time.strftime("%U", time.gmtime())
 next_week_num = time.strftime("%U", time.gmtime(time.time() + 7 * 24 * 60 * 60))
 
-
 b = Board("Test_board")
 
-# print(b)
 # print(*b.date_field, sep="\n")
-
-
-
-#print(get_dates_list())
+# print(get_dates_list())
